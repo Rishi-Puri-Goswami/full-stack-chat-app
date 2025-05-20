c537bfba4d7eb526ce365a647ba63eab422c19c2
Sahil Vaishnav🍊 <105405555+Sahil9079100@users.noreply.github.com>
2025-05-21 02:39:08 +0530

    Update user.route.js

--- chidiya_ghar/Backend/routes/user.route.js
+++ chidiya_ghar/Backend/routes/user.route.js
@@ -4,6 +4,7 @@ import {
   login,
   logout,
   signup,
+  test,
 } from "../controller/user.controller.js";
 import secureRoute from "../middleware/secureRoute.js";
 const router = express.Router();
@@ -13,5 +14,6 @@ router.post("/login", login);
 router.post("/logout", logout);
 // router.get("/allusers", secureRoute, allUsers);
 router.get("/allusers", allUsers);
+router.get("/test",test)
 
 export default router;
