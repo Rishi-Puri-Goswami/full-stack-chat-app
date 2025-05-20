c6fa4548ed73c88135b05e5a6efcd472a1e8d4aa
Rishi-Puri-Goswami <rishigithub9@gmail.com>
2025-05-21 04:17:29 +0530

    Update Logout.jsx

--- chidiya_ghar/Frontend/src/home/Leftpart/Logout.jsx
+++ chidiya_ghar/Frontend/src/home/Leftpart/Logout.jsx
@@ -10,7 +10,7 @@ function Logout() {
   const handleLogout = async () => {
     setLoading(true);
     try {
-      const res = await axios.post("/api/user/logout");
+      const res = await axios.post("https://chidyaghar-backend.onrender.com/api/user/logout");
       localStorage.removeItem("ChatApp");
       Cookies.remove("jwt");
       setLoading(false);
