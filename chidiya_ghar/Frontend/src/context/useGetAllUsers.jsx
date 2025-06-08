e845db437bd0e3ab8036664b94f0ee380ac5bbf3
Rishi-Puri-Goswami <rishigithub9@gmail.com>
2025-06-08 23:05:02 +0530

    Update useGetAllUsers.jsx

--- chidiya_ghar/Frontend/src/context/useGetAllUsers.jsx
+++ chidiya_ghar/Frontend/src/context/useGetAllUsers.jsx
@@ -1,32 +1,40 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import Cookies from "js-cookie";
 import axios from "axios";
+
 function useGetAllUsers() {
   const [allUsers, setAllUsers] = useState([]);
   const [loading, setLoading] = useState(false);
+
   useEffect(() => {
     const getUsers = async () => {
-      setLoading(true);
-      try {
-        const token = Cookies.get("jwt");
-        const response = await axios.get("https://chidyaghar-backend.onrender.com/api/user/allusers", {
-          credentials: "include",
+      const token = Cookies.get("jwt");
+      if (!token) {
+        console.warn("JWT token not found");
+        return;
+      }
 
-          
-    withCredentials: true , 
-  
+      setLoading(true);
+
+      try {
+        const response = await axios.get("https://chidyaghar-backend.onrender.com/api/user/allusers", {
+          withCredentials: true,
           headers: {
             Authorization: `Bearer ${token}`,
           },
         });
+
         setAllUsers(response.data);
-        setLoading(false);
       } catch (error) {
-        console.log("Error in useGetAllUsers: " + error);
+        console.error("Error in useGetAllUsers:", error);
+      } finally {
+        setLoading(false);
       }
     };
+
     getUsers();
   }, []);
+
   return [allUsers, loading];
 }
 
