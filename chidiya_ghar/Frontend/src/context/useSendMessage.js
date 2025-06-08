1b1531bc4278ab42b8da90de3457c1c08e14de73
Rishi-Puri-Goswami <rishigithub9@gmail.com>
2025-06-08 23:11:39 +0530

    Update useSendMessage.js

--- chidiya_ghar/Frontend/src/context/useSendMessage.js
+++ chidiya_ghar/Frontend/src/context/useSendMessage.js
@@ -1,25 +1,36 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import useConversation from "../zustand/useConversation.js";
 import axios from "axios";
+
 const useSendMessage = () => {
   const [loading, setLoading] = useState(false);
   const { messages, setMessage, selectedConversation } = useConversation();
+
   const sendMessages = async (message) => {
+    if (!selectedConversation || !selectedConversation._id) {
+      console.error("No conversation selected.");
+      return;
+    }
+
     setLoading(true);
     try {
       const res = await axios.post(
         `https://chidyaghar-backend.onrender.com/api/message/send/${selectedConversation._id}`,
-        { message }, {
-    withCredentials: true
-  }
+        { message },
+        {
+          withCredentials: true, 
+        }
       );
+
+      
       setMessage([...messages, res.data]);
-      setLoading(false);
     } catch (error) {
-      console.log("Error in send messages", error);
+      console.error("Error in sending message:", error.response?.data || error.message);
+    } finally {
       setLoading(false);
     }
   };
+
   return { loading, sendMessages };
 };
 
